b256799c892dc8fb727ca77f11ef63efa75c4044
michalmelis <michalmelis.sk@gmail.com>
2020-10-27 12:10:03 +0100

    First exercise done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+   public static void newArrayList(){
+       ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5));
+   }
+   public static void printNumbersORisEmpty(ArrayList<Integer> numbers){
+        if( numbers.isEmpty()) System.out.println("Is empty");
+       numbers.forEach((n) -> { System.out.println(n); });
+   }
+
 }
