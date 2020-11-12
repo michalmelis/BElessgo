5d95007a06cb30f57835214297f0a6432bdf4555
michalmelis <michalmelis.sk@gmail.com>
2020-11-12 21:56:37 +0100

    Working hard bro.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    public static List<Integer> numbers = new ArrayList<>();
+    public static List<Integer>   numbers = new ArrayList<>();
+    public static Set<Integer>        hashSetNumbers = new HashSet<>();
+    public static HashMap<String,Integer> hashMap = new HashMap<>();
 
     public static void main(String[] args)
     {
@@ -17,7 +17,24 @@ public class Main {
         System.out.println();
         addSomeNumbers();
         System.out.println();
-       multiplyByLength();
+        multiplyByLength();
+        System.out.println();
+        System.out.println(calculateBudget(getPeople()));
+        System.out.println(ifNameConstainsA(getPeople()));
+
+        hashSetNumbers.addAll(numbers);
+        hashSetNumbers.add(1);
+        hashSetNumbers.forEach(System.out::print);
+        System.out.println();
+        hashMap.put("Red",1);
+        hashMap.put("Green",2);
+        hashMap.put("Black",3);
+        hashMap.put("White",4);
+        System.out.println(hashMap);
+
+        ifDontExistAddElementToMap();
+
+
     }
 
 
@@ -44,7 +61,23 @@ public class Main {
         numbers.stream().mapToInt(integer -> integer*numbers.size()).forEach((System.out:: print));
 
     }
+    public static int calculateBudget(Person[] people){
+         return Arrays.stream(people).map(Person::getBudget).reduce(0, Integer::sum);
+    }
+    public static Person[] getPeople() {
+        Person person1 = new Person("John", 21, 29000);
+        Person person2 = new Person("Steve", 32, 32000);
+        Person person3 = new Person("Martin", 16, 1600);
+        return new Person[]{person1, person2, person3};
+    }
 
+    public static boolean ifNameConstainsA(Person[] people){
+        return Arrays.stream(people).map(Person::getName).anyMatch(i->i.contains("a"));
+    }
+   public static void ifDontExistAddElementToMap(){
+        hashMap.putIfAbsent("blue",5);
+        System.out.println(hashMap);
+   }
 }
 
 
---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    private String name;
+    private int age;
+    private int budget;
+
+    public Person(String name, int age, int budget){
+        this.name = name;
+        this.age = age;
+        this.budget = budget;
+    }
+    public int getBudget() {
+        return budget;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
